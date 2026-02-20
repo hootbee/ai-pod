2d76385636fe728601ee9c310b651f86218df710
hootbee <xx3xx27@gmail.com>
2026-02-20 18:04:07 +0900

    add: crawler 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CrawlerModule } from './modules/crawler/crawler.module';
 
 @Module({
-  imports: [],
+  imports: [CrawlerModule],
   controllers: [AppController],
   providers: [AppService],
 })
